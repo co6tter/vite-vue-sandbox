1231ce7e6ce65b6ae7b2f35d9c3b52eea1f98722
co6tter <co6tter@gmail.com>
2025-02-23 18:26:54 +0900

    feat: add Listbox

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import Menu from "./components/HeadlessUIMenu.vue";
+import HeadlessUIMenu from "@/components/HeadlessUIMenu.vue";
+import HeadlessUIListbox from "@/components/HeadlessUIListbox.vue";
 </script>
 
 <template>
-  <div>
-    <Menu />
+  <div class="container mx-auto p-8 flex flex-wrap gap-4">
+    <div class="min-w-80 min-h-80">
+      <HeadlessUIMenu />
+    </div>
+    <div class="min-w-80 min-h-80">
+      <HeadlessUIListbox />
+    </div>
   </div>
 </template>
 
--- src/components/HeadlessUIMenu.vue
+++ src/components/HeadlessUIMenu.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="fixed top-16 w-56 text-right">
+  <div class="text-right">
     <Menu as="div" class="relative inline-block text-left">
       <div>
         <MenuButton
           class="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
         >
           Options
+          <ChevronDownIcon
+            class="-mr-1 ml-2 h-5 w-5 text-violet-200 hover:text-violet-100"
+            aria-hidden="true"
+          />
         </MenuButton>
       </div>
 
@@ -83,6 +87,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import { ChevronDownIcon } from "@heroicons/vue/20/solid";
 </script>
