4ac50769aeabb67c8d9cd07bfc7ea94d52b06c61
co6tter <co6tter@gmail.com>
2025-04-03 12:08:49 +0900

    fix: add lib app page

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   <nav class="flex gap-4 text-stone-200 text-2xl p-4 justify-center">
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/headless-ui">Headless UI</RouterLink>
+    <RouterLink to="/lib-app">Lib App</RouterLink>
   </nav>
   <main>
     <routerView />
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 
 import HeadlessUI from "@/views/HeadlessUI.vue";
+import LibApp from "@/views/LibApp.vue";
 import TheHome from "@/views/TheHome.vue";
 
 const routes = [
   { path: "/", component: TheHome },
   { path: "/headless-ui", component: HeadlessUI },
+  { path: "/lib-app", component: LibApp },
 ];
 
 const router = createRouter({
